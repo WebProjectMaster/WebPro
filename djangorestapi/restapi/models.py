3bef17b8b2dee09cfcb34c0fd8c1c7554e74b586
Alekssey Orekhovv <aleoreh@gmail.com>
2017-07-23 15:32:50 +0300

    Adjust models.

--- djangorestapi/restapi/models.py
+++ djangorestapi/restapi/models.py
@@ -6,34 +6,54 @@ from django.db import models
 
 class Sites(models.Model):
     """ Сайты """
+
+    class Meta:
+        db_table = "sites"
+
     ID = models.AutoField(primary_key=True)
     Name = models.CharField(max_length=256)
 
 
 class Pages(models.Model):
     """ Страницы """
+
+    class Meta:
+        db_table = "pages"
+
     ID = models.AutoField(primary_key=True)
     Url = models.URLField("url", max_length=2048)
-    SiteID = models.ForeignKey(Sites, on_delete=models.CASCADE)
+    SiteID = models.ForeignKey(Sites, on_delete=models.CASCADE, db_column="SiteID")
     FoundDateTime = models.DateTimeField("date found")
     LastScanDate = models.DateTimeField("last scan date")
 
 
 class Persons(models.Model):
     """ Личности """
+
+    class Meta:
+        db_table = "persons"
+
     ID = models.AutoField(primary_key=True)
     Name = models.CharField(max_length=2048)
 
 
 class Keywords(models.Model):
     """ Ключевые слова """
+
+    class Meta:
+        db_table = "keywords"
+
     ID = models.AutoField(primary_key=True)
     Name = models.CharField(max_length=2048)
-    PersonID = models.ForeignKey(Persons)
+    PersonID = models.ForeignKey(Persons, db_column="PersonID")
 
 
 class PersonPageRank(models.Model):
     """ Ранг личностей на страницах """
-    PersonID = models.ForeignKey(Persons)
-    PageID = models.ForeignKey(Pages)
+
+    class Meta:
+        db_table = "person_page_rank"
+
+    PersonID = models.ForeignKey(Persons, db_column="PersonID")
+    PageID = models.ForeignKey(Pages, db_column="PageID")
     Rank = models.IntegerField()
